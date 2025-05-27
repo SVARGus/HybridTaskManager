e412d7db02a09b4780e9eebb8574c4946926b512
Ivan <ivansamiko1996@gmail.com>
2025-05-27 19:55:11 +0300

    Тест над подлючением ManageExistingTaskControl

--- HyrbridTaskManager/HybridTaskManager/HybridTaskManager/UserConrols/CalendarUI/CalendarUI.xaml.cs
+++ HyrbridTaskManager/HybridTaskManager/HybridTaskManager/UserConrols/CalendarUI/CalendarUI.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using HybridTaskManager.DataBaseSimulation;
+using HybridTaskManager.UserConrols.TaskManageControls;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,24 +23,71 @@ namespace HybridTaskManager.UserConrols.CalendarUI
     /// </summary>
     public partial class CalendarUI : UserControl
     {
+
+        private bool isInitialized = false;
+
         public CalendarUI()
         {
             InitializeComponent();
+            
             Loaded += CalendarUI_Loaded;
+
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            var manageTaskControl = new ManageExistingTaskControl(UserDataBase.Users[0]);
+
+            var window = new Window()
+            {
+                Title = "Редактирование задачи",
+                Content = manageTaskControl,
+                SizeToContent = SizeToContent.WidthAndHeight,
+                Owner = Window.GetWindow(this), 
+                WindowStartupLocation = WindowStartupLocation.CenterOwner,
+                ResizeMode = ResizeMode.NoResize
+            };
+
+            window.ShowDialog();
+        }
+
+        private static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj == null) yield break;
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                if (child is T t)
+                    yield return t;
+
+                foreach (T childOfChild in FindVisualChildren<T>(child))
+                    yield return childOfChild;
+            }
         }
 
         private void CalendarUI_Loaded(object sender, RoutedEventArgs e)
         {
+            if (isInitialized) return;
+            isInitialized = true;
+
             SetupWeekDays();
+
+            foreach (var btn in FindVisualChildren<Button>(ButtonBorder))
+            {
+                btn.Click += Btn_Click;
+            }
         }
 
         private void SetupWeekDays()
         {
             DateTime today = DateTime.Today;
             int currentDayIndex = (int)today.DayOfWeek;
-            currentDayIndex = currentDayIndex == 0 ? 7 : currentDayIndex; // Sunday => 7
+            currentDayIndex = currentDayIndex == 0 ? 7 : currentDayIndex; 
 
-            // Получаем Grid из Border
+            
             var dayGrid = DayBorder.Child as Grid;
             if (dayGrid == null) return;
 
