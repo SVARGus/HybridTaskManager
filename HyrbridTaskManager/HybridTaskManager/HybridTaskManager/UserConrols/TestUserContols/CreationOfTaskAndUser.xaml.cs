0cac4d6fba18529784725b7b91da020567e0c454
SVARGus <kyznetsovpn@mail.ru>
2025-05-27 21:33:13 +0300

    Доработка MainWindow: вставлены существующие UserControl и созданаы тестовые данные Исправления KanbanBoardControl и вложенных UserControl

--- HyrbridTaskManager/HybridTaskManager/HybridTaskManager/UserConrols/TestUserContols/CreationOfTaskAndUser.xaml.cs
+++ HyrbridTaskManager/HybridTaskManager/HybridTaskManager/UserConrols/TestUserContols/CreationOfTaskAndUser.xaml.cs
@@ -15,30 +15,30 @@ namespace HybridTaskManager.UserConrols.TestUserContols
         User CurrentUser = new User("Серёга",new UserRole("Администратор",true,true));
         public CreationOfTaskAndUser()
         {
-            InitializeComponent();
-            ChosenTaskStatus.ItemsSource = StatusTypeDataBase.TaskStatuses;
-            NewTasksType.ItemsSource = TaskTypesDataBase.TaskTypes;
-            TaskPerformer.ItemsSource = UserDataBase.Users;
-            CurrentTasks.ItemsSource = TaskDataBase.TaskBase;
-            Belonging.ItemsSource = ProjectDataBase.Projects;
-            NewTasksPriority.ItemsSource = TaskPriorityDataBase.TaskPriorities;
+            //InitializeComponent();
+            //ChosenTaskStatus.ItemsSource = StatusTypeDataBase.TaskStatuses;
+            //NewTasksType.ItemsSource = TaskTypesDataBase.TaskTypes;
+            //TaskPerformer.ItemsSource = UserDataBase.Users;
+            //CurrentTasks.ItemsSource = TaskDataBase.TaskBase;
+            //Belonging.ItemsSource = ProjectDataBase.Projects;
+            //NewTasksPriority.ItemsSource = TaskPriorityDataBase.TaskPriorities;
         }
 
         private void AddTaskButton_Click(object sender, RoutedEventArgs e)
         {
-            Factories.CastomTaskFactory factory = new Factories.CastomTaskFactory();
-            TaskItem newTask = factory.CreateTask(NewTasksName.Text,
-                                            TaskDescription.Text,
-                                            Belonging.SelectedItem as Project,
-                                            ChosenTaskStatus.SelectedItem as DTO.DictionaryEntity.TaskStatus,
-                                            NewTasksType.SelectedItem as TaskType,
-                                            NewTasksPriority.SelectedItem as TaskPriority,
-                                            CurrentUser,
-                                            TaskPerformer.SelectedItem as User,
-                                            BeginDate.SelectedDate.Value,
-                                            DeadLineDate.SelectedDate.Value
-                                            );
-            TaskDataBase.TaskBase.Add(newTask);
+            //Factories.CastomTaskFactory factory = new Factories.CastomTaskFactory();
+            //TaskItem newTask = factory.CreateTask(NewTasksName.Text,
+            //                                TaskDescription.Text,
+            //                                Belonging.SelectedItem as Project,
+            //                                ChosenTaskStatus.SelectedItem as DTO.DictionaryEntity.TaskStatus,
+            //                                NewTasksType.SelectedItem as TaskType,
+            //                                NewTasksPriority.SelectedItem as TaskPriority,
+            //                                CurrentUser,
+            //                                TaskPerformer.SelectedItem as User,
+            //                                BeginDate.SelectedDate.Value,
+            //                                DeadLineDate.SelectedDate.Value
+            //                                );
+            //TaskDataBase.TaskBase.Add(newTask);
         }
     }
 }
