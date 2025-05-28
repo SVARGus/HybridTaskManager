3f76f5245e2529bc347e532f50174fbe28d68cdb
Ivan <ivansamiko1996@gmail.com>
2025-05-28 16:52:39 +0300

    Удалил FindVisualChildren +Проверка

--- HyrbridTaskManager/HybridTaskManager/HybridTaskManager/UserConrols/CalendarUI/CalendarUI.xaml.cs
+++ HyrbridTaskManager/HybridTaskManager/HybridTaskManager/UserConrols/CalendarUI/CalendarUI.xaml.cs
@@ -225,20 +225,7 @@ namespace HybridTaskManager.UserConrols.CalendarUI
 
         
 
-        private static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
-        {
-            if (depObj == null) yield break;
-
-            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
-            {
-                DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
-                if (child is T t)
-                    yield return t;
-
-                foreach (T childOfChild in FindVisualChildren<T>(child))
-                    yield return childOfChild;
-            }
-        }
+        
 
         private DateTime GetStartOfWeek(DateTime date)
         {
