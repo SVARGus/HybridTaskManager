c6615a8ed5c6f5f8eacc72e0e00c5b9693399783
Your NameMadeiveTheMagician <madeive.the.magician@gmail.comyou@example.com>
2025-05-30 11:58:18 +0300

    Слияние интерпритаций. Этап 1.

--- HyrbridTaskManager/HybridTaskManager/HybridTaskManager/UserConrols/EisenhoverMatrix/EisenhowerMatrix.xaml.cs
+++ HyrbridTaskManager/HybridTaskManager/HybridTaskManager/UserConrols/EisenhoverMatrix/EisenhowerMatrix.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace HybridTaskManager.UserControl
+namespace HybridTaskManager.UserControls
 {
     /// <summary>
     /// Логика взаимодействия для EisenhowerMatrix.xaml
--- HyrbridTaskManager/HybridTaskManager/HybridTaskManager/UserConrols/KanbanBoardControl.xaml.cs
+++ HyrbridTaskManager/HybridTaskManager/HybridTaskManager/UserConrols/KanbanBoardControl.xaml.cs
@@ -1,26 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Net.NetworkInformation;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using HybridTaskManager.DataBaseSimulation;
 using HybridTaskManager.DTO.DictionaryEntity;
 using TaskStatus = HybridTaskManager.DTO.DictionaryEntity.TaskStatus;
 
 namespace HybridTaskManager.UserConrols
 {
-    public partial class KanbanBoardControl : UserControl
+    public partial class KanbanBoardControl : System.Windows.Controls.UserControl
     {
         public ObservableCollection<StatusColumnViewModel> StatusColumns { get; } = new ObservableCollection<StatusColumnViewModel>();
 
